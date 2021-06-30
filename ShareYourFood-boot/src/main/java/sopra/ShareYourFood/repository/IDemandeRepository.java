9dce52b79cf6c8f9a33f0c60c77d67cb332c9645
Seif <seif@>
2021-06-30 07:55:26 +0200

    mini corrections

--- ShareYourFood-boot/src/main/java/sopra/ShareYourFood/repository/IDemandeRepository.java
+++ ShareYourFood-boot/src/main/java/sopra/ShareYourFood/repository/IDemandeRepository.java
@@ -78,6 +78,7 @@ public interface IDemandeRepository extends JpaRepository<Demande, Long> {
 			+ " and d.entite.id = :id")
 	List<Demande> findAllDonneEtDemandeArchiveeByEntiteById(@Param("id") Long id);
 	
+	
 	//DASHBOARD DONNEUR
 	@Transactional
 	@Modifying
@@ -94,10 +95,7 @@ public interface IDemandeRepository extends JpaRepository<Demande, Long> {
 	@Query("select d.lot.don.entite.nom from Demande d where d.id = :id")
 	Optional<String> findNomEntiteByDemandeId(@Param("id") Long id);
 	
-	@Query("select distinct d from Demande d where d.lot.statut = sopra.ShareYourFood.model.Statut.DONNE "
-			+ "and  d.statutNotif = sopra.ShareYourFood.model.StatutNotif.ARCHIVER"
-			+ " and d.entite.id = :id")
-	List<Demande> findAllDonneEtDemandeArchiveeByEntiteById(@Param("id") Long id);
+	
 	
 	
 }
